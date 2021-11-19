62e8d5d3e86c60710b2a696df18d6f5cb6245c32
a.karshakevich <a.karshakevich@richbrains.net>
2021-11-19 17:06:54 +0300

    Created admin panel

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -2,7 +2,38 @@
 
 namespace App\Controller\Admin;
 
-class SecurityController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin")
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="admin_security_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_dashboard_show');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('admin/security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="admin_security_logout")
+     */
+    public function logout(): void
+    {
+//            return $this->redirectToRoute('admin_security_login');
+    }
 }
