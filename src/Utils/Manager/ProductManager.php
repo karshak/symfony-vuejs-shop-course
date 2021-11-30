c25de6419bed28e4d5938716192f9cb36e4b43bc
Andrew <a.karashakevich@richbrains.net>
2021-11-30 18:14:12 +0300

    added AbsttractBaseManage and uuid

--- src/Utils/Manager/ProductManager.php
+++ src/Utils/Manager/ProductManager.php
@@ -6,13 +6,8 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectRepository;
 
-class ProductManager
+class ProductManager extends AbstractBaseManager
 {
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
     /**
      * @var string
      */
@@ -30,7 +25,7 @@ class ProductManager
      */
     public function __construct(EntityManagerInterface $entityManager, ProductImageManager $productImageManager, string $productImagesDir)
     {
-        $this->entityManager = $entityManager;
+        parent::__construct($entityManager);
         $this->productImagesDir = $productImagesDir;
         $this->productImageManager = $productImageManager;
     }
@@ -44,18 +39,9 @@ class ProductManager
     }
 
     /**
-     * @param Product $product
+     * @param object $product
      */
-    public function save(Product $product)
-    {
-        $this->entityManager->persist($product);
-        $this->entityManager->flush();
-    }
-
-    /**
-     * @param Product $product
-     */
-    public function remove(Product $product)
+    public function remove(object $product)
     {
         $product->setIsDeleted(true);
         $this->save($product);
